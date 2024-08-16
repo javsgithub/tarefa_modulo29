e354a325ed0312e85ee9032dfddf4785c24fbb98
Jefferson Santos <doctorjeffbr@yahoo.com.br>
2024-08-16 17:30:57 -0300

    enviando tarefa domódulo 29

---
+++ src/components/FormVagas/index.tsx
@@ -0,0 +1,30 @@
+import { FormEvent, useState } from 'react'
+
+import { FormVagas as Form } from './styles'
+
+type Props = {
+  aoPesquisar: (termo: string) => void
+}
+
+const FormVagas = ({ aoPesquisar }: Props) => {
+  const [termo, setTermo] = useState<string>('')
+
+  const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    aoPesquisar(termo.toLocaleLowerCase())
+  }
+
+  return (
+    <Form onSubmit={aoEnviarForm}>
+      <input
+        placeholder="Front-end, fullstack, node, design"
+        onChange={(e) => setTermo(e.target.value)}
+        type="search"
+      />
+      <button type="submit">
+        Pesquisar
+      </button>
+    </Form>
+  )
+}
+export default FormVagas
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from "../../styles"
+import { Hero as Banner } from "./styles"
+
+const Hero = () => (
+  <Banner>
+    <Container>
+      <div>
+        <h2>
+          As melhores vagas para tecnologia, design e artes visuais.
+        </h2>
+      </div>
+    </Container>
+  </Banner>
+)
+
+export default Hero
